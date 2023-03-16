48ceb1b51298bd3ffd75e3ea592f2930b2c703fe
Pouilly Oceane <oceane.pouilly62@gmail.com>
2023-03-16 23:00:03 +0100

    :fire: Question 4 et 5 TP1 fini

--- src/main/java/com/example/tp1/Main.java
+++ src/main/java/com/example/tp1/Main.java
@@ -4,19 +4,18 @@ package com.example.tp1;
 import com.example.tp1.entities.E_Competition;
 import com.example.tp1.entities.E_Coureur;
 import com.example.tp1.entities.E_Course;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
+import jakarta.persistence.*;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.sql.Date;
 import java.util.Arrays;
 
+import static java.lang.Thread.getAllStackTraces;
 import static java.lang.Thread.sleep;
 
 public class Main {
+
     public static void main(String[] args) throws ParseException {
         EntityManagerFactory entityManagerFactory = null;
         EntityManager entityManager = null;
@@ -24,7 +23,7 @@ public class Main {
             entityManagerFactory = Persistence.createEntityManagerFactory("default");
             entityManager = entityManagerFactory.createEntityManager();
 
-            System.out.println("------------------------------------");
+            System.out.println("--------------- TP1 --------------------- \n");
 
           //  E_Course course = entityManager.find(E_Course.class,1);
           //  System.out.println(course);
@@ -38,23 +37,32 @@ public class Main {
             System.out.println("Avant modification");
             System.out.println(competition.toString());
 
-            sleep(7000); // Le temps de faire la modification en BDD
+           // sleep(7000); // Le temps de faire la modification en BDD
             entityManager.refresh(competition); // rafraîchir les rôles
 
             // Affichage 2
             System.out.println("Après modification");
             System.out.println(competition.toString());
 
+            System.out.println("--------------------------------------------- \n");
+            System.out.println("--------------- TP2 --------------------- \n");
+            System.out.println("Nombre de kilomètres : "+getDistanceComp(entityManager,1));
 
 
             //     System.out.println(competition);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } finally {
+        }  finally {
             if (entityManager != null) entityManager.close();
             if (entityManagerFactory != null) entityManagerFactory.close();
         }
 
+    }
+
+    private static double getDistanceComp(EntityManager entityManager, int idComp) {
+        final String strQuery = "SELECT SUM (e.distance) FROM E_Course e WHERE e.competition.id = :idComp";
+        Query query = entityManager.createQuery(strQuery);
+        query.setParameter("idComp", idComp);
+        double distance = (double) query.getSingleResult();
+        return distance;
     }
 }
 
